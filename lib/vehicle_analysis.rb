e0e039d9f22577b5db937746198da5e4c65863d0
Justin <justin@baal-software.com>
2019-03-27 12:15:04 -0600

    Refactor gnarly if statement into readable code

--- lib/vehicle_analysis.rb
+++ lib/vehicle_analysis.rb
@@ -3,31 +3,10 @@
 class VehicleAnalysis
 
   def analyze(vehicle)
-    if vehicle.car?
-      if vehicle.four_wheel_drive? || !vehicle.four_wheel_drive?
-        puts "Vehicle has four wheels "
-        if vehicle.four_wheel_drive?
-          puts "with four wheel drive"
-        else
-          puts "with two wheel drive"
-        end
-      end
-    elsif vehicle.tractor?
-      puts "Vehicle has four wheels "
-      if vehicle.big_back_wheels?
-        puts "with big wheels in the back"
-      end
-    elsif vehicle.pickup?
-      puts "Vehicle has four wheels "
-      if vehicle.four_wheel_drive?
-        puts "with four wheel drive"
-      else
-        puts "with two wheel drive"
-      end
-      if vehicle.big_back_wheels?
-        puts "with big wheels in the back"
-      end
-    end
+    puts "Vehicle has four wheels "
+    puts "with four wheel drive" if vehicle.four_wheel_drive? unless vehicle.tractor?
+    puts "with two wheel drive" unless vehicle.four_wheel_drive? unless vehicle.tractor?
+    puts "with big wheels in the back" if vehicle.big_back_wheels? unless vehicle.car? 
   end
 
 end
